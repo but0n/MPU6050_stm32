e858a74b5604036034ac45c0103d5dcdd4582c00
but0n <but0n@icloud.com>
2016-08-03 23:14:27 +0800

    :v: Test

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "stm32f10x.h"
+#include "dmp.h"
 
 #define BITBAND(addr, bitnum) ((addr & 0xF0000000) + 0x2000000 + ((addr & 0xFFFFF) << 5) + (bitnum<<2))
 #define BIT_ADDR(addr, bitnum) *((volatile unsigned long *)(BITBAND(addr, bitnum)))
@@ -271,6 +272,7 @@ int main() {
 
     MPU_init();
 
+    MPU-dmpInit();
 
     while(1) {
         //GYRO_x = MPU_GetData(ACCEL_XOUT_H);
