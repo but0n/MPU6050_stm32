e8acd12f054c695e7cdc66c237b9adb1949d2d42
but0n <but0n@icloud.com>
2016-08-10 23:24:45 +0800

    Changed I2C Connect SDA PIN from B13 to B14

--- libs/STM32_USB-FS-Device_Lib_V4.0.0/Libraries/STM32F10x_StdPeriph_Driver/src/i2c.c
+++ libs/STM32_USB-FS-Device_Lib_V4.0.0/Libraries/STM32F10x_StdPeriph_Driver/src/i2c.c
@@ -9,18 +9,18 @@ void delay_us(volatile unsigned int nus) {
 
 void IIC_init() {
     RCC->APB2ENR |= 1<<3;       //GPIOB enable
-    GPIOB->CRH &= 0x0F0FFFFF;   //reset B13 & B15
-    GPIOB->CRH |= 0x70700000;   //Set B13 & B15 b0111
+    GPIOB->CRH &= 0x00FFFFFF;   //reset B13 & B15
+    GPIOB->CRH |= 0x77000000;   //Set B13 & B15 b0111
     GPIOB->ODR |= 5<<5;
 
-//这里B13和B15设置为开漏输出模式(01), 其实一般都用推挽输出模式。
+//这里B14和B15设置为开漏输出模式(01), 其实一般都用推挽输出模式。
 //开漏输出模式有个"好的Bug": 在输出状态下，输入信号没有关断，后面的路也是畅通的
 //最终就会进入输入寄存器，导致在输出状态下，输入功能保持正常。
 //如果用推挽输出的话，IIC通讯时每次读取SDA之前都要配置SDA为输入，才能读取IDR
 
     RCC->APB2ENR |= 1<<4;       //GPIOC enable
     GPIOB->CRL &= 0xF0FFFFFF;
-    GPIOB->CRL |= 0x03000000;   //推挽输出, AD0
+    GPIOB->CRL |= 0x03000000;   //推挽输出, AD0 ----> PC6
     AD0 = 0;
 }
 void IIC_Start() {
